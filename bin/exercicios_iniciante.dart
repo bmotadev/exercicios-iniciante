77cf349d401e7569e69c89192ea0c602e5f01e7b
Bruno Mota <bmotadev@gmail.com>
2023-02-18 19:25:58 -0300

    :tada: Commit inicial

---
+++ bin/exercicios_iniciante.dart
@@ -0,0 +1,5 @@
+import 'package:exercicios_iniciante/exercicios_iniciante.dart' as exercicios_iniciante;
+
+void main(List<String> arguments) {
+  print('Hello world: ${exercicios_iniciante.calculate()}!');
+}
---
+++ test/exercicios_iniciante_test.dart
@@ -0,0 +1,8 @@
+import 'package:exercicios_iniciante/exercicios_iniciante.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
