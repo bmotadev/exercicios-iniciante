03d40a7312c14e45284e48fba4d56a1c317df49d
Bruno Mota <bmotadev@gmail.com>
2023-02-18 20:45:58 -0300

    :recycle: refactor: Validando valores nulos - Exercicio 1002, adiciona validação para valor nulo

--- lib/src/1002.dart
+++ lib/src/1002.dart
@@ -2,10 +2,22 @@ import 'dart:io';
 import 'dart:math';
 
 exercicioMilEDois() {
-  double raio = double.parse(stdin.readLineSync()!);
-  double n = 3.14159;
+  // double raio = double.parse(stdin.readLineSync()!); // ! - estamos garantindo que nunca vai ser nula
+  // double n = 3.14159;
 
-  double area = n * (pow(raio, 2));
+  // double area = n * (pow(raio, 2));
+
+  // print("A=${area.toStringAsFixed(4)}");
+
+  double raioTeste = 0.0;
+  final String? leitura = stdin.readLineSync();
+    if(leitura != null) { // validação para o valor não ser nulo
+      raioTeste = double.parse(leitura);
+    }
+  double n = 3.14159;
+  double area =  n * (pow(raioTeste, 2));
 
   print("A=${area.toStringAsFixed(4)}");
+
+
 }
